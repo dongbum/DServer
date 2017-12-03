82803470990731a82317335c4bc0a267810d9077
DONGBUM, KIM <dongbum9@gmail.com>
2017-12-04 00:42:21 +0900

    윈도우에서는 critical_section을 사용하고 다른 운영체제에서는 std::mutex를 사용하도록 커스텀락 클래스 수정

--- src/dserver/lock/custom_lock.h
+++ src/dserver/lock/custom_lock.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#ifdef _WIN32
+#include <Windows.h>
+
 class CustomLock
 {
 public:
@@ -24,5 +27,31 @@ public:
 	}
 private:
 	CRITICAL_SECTION	cs;
+};
+#else
+class CustomLock
+{
+public:
+	CustomLock(void)
+	{
+
+	}
+
+	virtual ~CustomLock(void)
+	{
+
+	}
+
+	void lock(void)
+	{
+		mutex_.lock();
+	}
+
+	void unlock(void)
+	{
+		mutex_.unlock();
+	}
+private:
 	std::mutex			mutex_;
 };
+#endif
