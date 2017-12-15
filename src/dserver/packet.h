4c6e20e5cabd3afdbabdcdcb57ea9885646f8f21
DONGBUM, KIM <dongbum9@gmail.com>
2017-12-15 12:26:15 +0900

    패킷 클래스 수정 패킷 클래스 프로젝트에 추가

--- src/dserver/packet.h
+++ src/dserver/packet.h
@@ -9,7 +9,11 @@ public:
 		end_pos_ = &buffer_[SEND_BUFFER_SIZE];
 	}
 
-	virtual ~Packet(void);
+	virtual ~Packet(void)
+	{
+
+	}
+
 	void WriteData(const void* buffer, int size)
 	{
 		if (write_pos_ + size > end_pos_)
@@ -17,12 +21,15 @@ public:
 
 		memcpy(write_pos_, buffer, size);
 		write_pos_ += size;
+
+		data_size_ += size;
 	}
 
 	template<typename T>
 	Packet& operator << (T source)
 	{
 		WriteData(&source, sizeof(T));
+		return *this;
 	}
 
 	template<typename T>
@@ -31,8 +38,19 @@ public:
 
 	}
 
+	size_t GetSize(void)
+	{
+		return data_size_;
+	}
+
+	char* GetBuffer(void)
+	{
+		return buffer_;
+	}
+
 private:
 	char	buffer_[SEND_BUFFER_SIZE];
 	char*	write_pos_;
 	char*	end_pos_;
+	size_t	data_size_;
 };
