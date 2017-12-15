98962e7f81d1d2d87f8281e9d3b136c9977f3d92
DONGBUM, KIM <dongbum9@gmail.com>
2017-12-15 12:26:28 +0900

    패킷 클래스 적용

--- src/game_server/game_protocol.cpp
+++ src/game_server/game_protocol.cpp
@@ -20,23 +20,17 @@ uint32_t GameProtocol::Echo(std::shared_ptr<BasicSocket> session, uint32_t& prot
 {
 	//std::cout << "Protocol Echo" << std::endl;
 	
-	char send_buffer[SEND_BUFFER_SIZE] = { 0, };
-	int index = 0;
-
 	int test = 7;
 
 	Header send_header;
 	send_header.SetDataLength(4);
 	send_header.SetTotalLength(16);
 
-	memcpy(&send_buffer[index], &send_header, sizeof(send_header));
-	index += sizeof(send_header);
-	memcpy(&send_buffer[index], &test, sizeof(test));
-	index += sizeof(test);
+	Packet packet;
+	packet << send_header;
+	packet << test;
 
-	int send_length = index;
-
-	session->OnSend(send_length, send_buffer);
+	session->OnSend(packet.GetSize(), packet.GetBuffer());
 
 	return 0;
 }
