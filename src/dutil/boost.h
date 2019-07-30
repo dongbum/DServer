68bb215ec64e9dd1f8acf8342f9b13486f8c4fa1
DONGBUM, KIM <dongbum9@gmail.com>
2019-07-30 13:18:31 +0900

    SSL 비활성화 코드 추가

--- src/dutil/boost.h
+++ src/dutil/boost.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <boost/asio.hpp>
-#include <boost/asio/ssl.hpp>
 #include <boost/thread.hpp>
 #include <boost/format.hpp>
 #include <boost/filesystem.hpp>
@@ -14,17 +13,21 @@
 #include <boost/pool/singleton_pool.hpp>
 #include <boost/pool/pool_alloc.hpp>
 
+#ifdef USE_SSL
+#include <boost/asio/ssl.hpp>
+#endif
+
 #define TCP_V4	boost::asio::ip::address_v4::any()
 #define TCP_V6	boost::asio::ip::address_v6::any()
 
 using Strand = boost::asio::io_context::strand;
 using IoContext = boost::asio::io_context;
-using SSLContext = boost::asio::ssl::context;
 using EndPoint = boost::asio::ip::tcp::endpoint;
 using Acceptor = boost::asio::ip::tcp::acceptor;
 using TCPSocket = boost::asio::ip::tcp::socket;
 #ifdef USE_SSL
 using Socket = boost::asio::ssl::stream<TCPSocket>;
+using SSLContext = boost::asio::ssl::context;
 #else
 using Socket = TCPSocket;
 #endif
