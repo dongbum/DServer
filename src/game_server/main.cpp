78afe51ac83ffadaf29b85992c75d26b01598ab2
DONGBUM, KIM <dongbum9@gmail.com>
2019-09-29 16:45:23 +0900

    #7 Enable minidump only on Windows.

--- src/game_server/main.cpp
+++ src/game_server/main.cpp
@@ -8,9 +8,11 @@ int main(void)
 	if (false == LogManager::GetInstance()->Init())
 		return 0;
 
+#ifdef _WIN32
 	Minidump::Init(
 		ConfigManager::GetInstance()->GetString("DServer", "DUMP_PATH"),
 		ConfigManager::GetInstance()->GetString("DServer", "DUMP_FILENAME"));
+#endif
 
 	std::shared_ptr<GameServer> game_server(new GameServer);
 	GameServer::SetServerInstance(std::static_pointer_cast<Server<GameUser>>(game_server));
